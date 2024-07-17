1a1a33364263576834d8a6e38670aa3caab9f548
TNemoo <k9657602525@gmail.com>
2024-07-17 21:27:24 +0300

    Retry Spring, Circuit Breaker are working. SS is still not working.

--- contact-service/src/main/java/com/javaguru/contactservice/service/PhoneCodeServiceClient.java
+++ contact-service/src/main/java/com/javaguru/contactservice/service/PhoneCodeServiceClient.java
@@ -1,10 +1,8 @@
 package com.javaguru.contactservice.service;
 
-import feign.Request;
-import feign.Retryer;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,18 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name = "PHONE-CODE-SERVICE")
 public interface PhoneCodeServiceClient {
 
-    //аннотация CircuitBreaker, name = "phone-code-service" соответствует имени в yml (instances: phone-code-service:)
-    @CircuitBreaker(name = "phone-code-service", fallbackMethod = "getIdByCodeFallBack")
-    @GetMapping("/codes")
-    Integer getIdByCode(@RequestParam("code") Integer code);
-
-    // метод, который CircuitBreaker будет выполнять в случае отказа МС PHONE-CODE-SERVICE:
-    default Integer getIdByCodeFallBack(Integer code, Throwable throwable) {
-        System.out.println("   code: " + code + ", exception: " + throwable.getMessage());
-        return Integer.MAX_VALUE;
-    }
-
-
+    //если fallbackMethod не указывать, то он будет соответствовать указанному в CircuitBreaker
+    @Retry(name = "phone-code-service", fallbackMethod = "getCodeByIdFallBack2")
     //аннотация CircuitBreaker, name = "phone-code-service" соответствует имени в yml (instances: phone-code-service:)
     @CircuitBreaker(name = "phone-code-service", fallbackMethod = "getCodeByIdFallBack")
     @GetMapping("/codes/{id}")
@@ -32,6 +20,26 @@ public interface PhoneCodeServiceClient {
     // метод, который CircuitBreaker будет выполнять в случае отказа МС PHONE-CODE-SERVICE:
     default Integer getCodeByIdFallBack(Integer id, Throwable throwable) {
         System.out.println("   code: " + id + ", exception: " + throwable.getMessage());
+        return Integer.MAX_VALUE;
+    }
+
+    // метод, который CircuitBreaker будет выполнять в случае отказа МС PHONE-CODE-SERVICE:
+    default Integer getCodeByIdFallBack2(Integer code, Throwable throwable) {
+        System.out.println("   code: " + code + ", exception: " + throwable.getMessage());
+        return 999_999;
+    }
+
+
+    //аннотация Retry ограничивает количество запросов в сек., посылая дефолтный метод getIdByCodeFallBack2 при переполнении
+    @Retry(name = "phone-code-service")
+    //аннотация CircuitBreaker, name = "phone-code-service" соответствует имени в yml (instances: phone-code-service:)
+    @CircuitBreaker(name = "phone-code-service", fallbackMethod = "getIdByCodeFallBack")
+    @GetMapping("/codes")
+    Integer getIdByCode(@RequestParam("code") Integer code);
+
+    // метод, который CircuitBreaker будет выполнять в случае отказа МС PHONE-CODE-SERVICE:
+    default Integer getIdByCodeFallBack(Integer code, Throwable throwable) {
+        System.out.println("   code: " + code + ", exception: " + throwable.getMessage());
         return Integer.MAX_VALUE;
     }
 }
